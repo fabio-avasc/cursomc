fdaa73fbf6161f2abf474ceb5f530857ac700558
Fabio Antunes <fabio.avasc@outlook.com>
2019-10-14 19:43:35 -0300

    Produto e associação ManyToMany

--- src/main/java/com/fabioantunes/cursomc/repositories/ProdutoRepository.java
+++ src/main/java/com/fabioantunes/cursomc/repositories/ProdutoRepository.java
@@ -3,10 +3,10 @@ package com.fabioantunes.cursomc.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.fabioantunes.cursomc.domain.Categoria;
+import com.fabioantunes.cursomc.domain.Produto;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+public interface ProdutoRepository  extends JpaRepository<Produto, Integer>{
 
 	
 	
