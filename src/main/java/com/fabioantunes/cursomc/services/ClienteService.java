451473e511963a50e0f6cf8c19ef83196fd455c0
Fabio Antunes <fabio.avasc@outlook.com>
2019-10-17 12:13:02 -0300

    Endpoint /clientes/{id}

---
+++ src/main/java/com/fabioantunes/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.fabioantunes.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fabioantunes.cursomc.domain.Cliente;
+import com.fabioantunes.cursomc.repositories.ClienteRepository;
+import com.fabioantunes.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " 
+		+ Cliente.class.getName() ));
+	}
+}
