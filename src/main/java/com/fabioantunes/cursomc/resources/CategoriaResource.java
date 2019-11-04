fe932cc5f4f63428f66a2ebb532ebcd4c913f2e5
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-04 17:55:39 -0300

    Testte

--- src/main/java/com/fabioantunes/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/fabioantunes/cursomc/resources/CategoriaResource.java
@@ -34,6 +34,7 @@ public class CategoriaResource {
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+		
 	}
 	
 }
