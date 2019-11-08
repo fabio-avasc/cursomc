b211ef5d49221dccc705a1c412ca1a80c97e571f
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-08 12:06:40 -0300

    Validação Sintática

--- src/main/java/com/fabioantunes/cursomc/services/CategoriaService.java
+++ src/main/java/com/fabioantunes/cursomc/services/CategoriaService.java
@@ -3,6 +3,8 @@ package com.fabioantunes.cursomc.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
@@ -11,6 +13,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fabioantunes.cursomc.domain.Categoria;
+import com.fabioantunes.cursomc.dto.CategoriaDTO;
 import com.fabioantunes.cursomc.repositories.CategoriaRepository;
 import com.fabioantunes.cursomc.services.exceptions.DataIntegrityException;
 import com.fabioantunes.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +56,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+
+	public Categoria fromDTO(@Valid CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
