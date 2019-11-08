e59eb33929936b1fceae0821e37f31cc429856c2
Fabio Antunes de Vasconcelos <fabioantunes@brq.com>
2019-11-08 15:44:34 -0300

    Retorno da Validação personalizado e ajustado

---
+++ src/main/java/com/fabioantunes/cursomc/services/exceptions/ValidationError.java
@@ -0,0 +1,30 @@
+package com.fabioantunes.cursomc.services.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fabioantunes.cursomc.resources.FieldMessage;
+import com.fabioantunes.cursomc.resources.exception.StandardError;
+
+public class ValidationError extends StandardError {
+
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public ValidationError(Integer status, String msg, Long timeStamp) {
+		super(status, msg, timeStamp);
+	}
+	
+	public List<FieldMessage> getErros(){
+		return errors;
+	}
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+	
+	
+}
